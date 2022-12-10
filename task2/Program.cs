7d70bb745fcc27d6bfda16ab1465b177e6c81951
RizvanVeliyev <veliyevrizvan250@gmail.com>
2022-12-10 20:09:05 +0400

    Add project files.

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace task2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Hello World-task1
+			Console.Write("a:");
+			int a = Convert.ToInt32(Console.ReadLine());
+			Console.Write("b:");
+			int b = Convert.ToInt32(Console.ReadLine());
+			int total;
+			total = a + b;
+			Console.Write("cem:");
+			Console.WriteLine(total);
+		}
+	}
+}
